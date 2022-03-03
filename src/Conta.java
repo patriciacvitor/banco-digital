9c2d7c3115f183b720d084fe330feca7fafc4fe5
Patrícia Vitor <82560461+patriciacvitor@users.noreply.github.com>
2022-03-03 16:53:33 -0300

    first commit

---
+++ src/Conta.java
@@ -0,0 +1,53 @@
+public abstract class Conta implements IConta {
+
+    private static final int AGENCIA_PADRAO = 1;
+
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    protected void imprimirInformacoesEmComum() {
+        System.out.printf("Titular: %s \n", this.cliente.getNome());
+        System.out.printf("Agencia: %d \n", this.agencia);
+        System.out.printf("Conta: %d \n", this.numero);
+        System.out.printf("Saldo: %.2f \n", this.saldo);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cliente cliente1 = new Cliente();
+        cliente1.setNome("Patrícia");
+
+        IConta contaCorrente = new ContaCorrente(cliente1);
+        contaCorrente.depositar(100);
+        Conta contaPoupanca = new ContaPoupanca(cliente1);
+        contaPoupanca.depositar(300);
+
+        contaCorrente.imprimirExtrato();
+        contaPoupanca.imprimirExtrato();
+
+        contaCorrente.transferir(50, contaPoupanca);
+
+        contaCorrente.imprimirExtrato();
+        contaPoupanca.imprimirExtrato();
+
+
+    }
+}
+
